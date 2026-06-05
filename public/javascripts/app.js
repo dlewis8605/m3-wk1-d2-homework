323d97738b47ec6bd1d66b5b31069c138b0b55bc
dlewis8605 <d.lewis.8605@westcliff.edu>
2026-06-04 19:57:19 -0700

    Final Push

---
+++ public/javascripts/app.js
@@ -0,0 +1,101 @@
+// The navigation component
+Vue.component('nav-menu', {
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-dark custom-nav shadow-sm mb-5">
+        <div class="container">
+            <a class="navbar-brand" href="#">Food Blog</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Recipes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lifestyles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    `
+});
+
+// The blog post component
+Vue.component('blog-post', {
+    props: ['post'],
+    template: `
+    <div class="card post-card shadow-sm">
+        <div class="card-body">
+            <img :src="post.profilePic" class="profile-pic mr-4" alt="Profile" @click="$emit('show-profile', post)">
+            <h4 class="card-title post-author mb-1">{{ post.author }}</h4>
+            <h6 class="card-subtitle post-date mb-3">{{ post.date }}</h6>
+            <p class="card-text post-text">
+                {{ post.text }}
+            </p>
+            <div class="text-right mt-3">
+                <button class="btn btn-outline-success font-weight-bold" style="border-radius: 20px; padding: 5px 20px;">REPLY</button>
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>
+    `
+});
+
+// Main Vue instance
+new Vue({
+    el: '#app',
+    data: {
+        chiliImage: 'images/chili.jpg',
+        activePost: null,
+        posts: {
+            post1: {
+                author: 'Brianna',
+                date: 'February 18, 2021 @ 3:30 pm',
+                text: 'Was amazing! My Walmart didn’t have coriander in stock and didn’t have ground cumin. I used serrano instead of jalapeño. It was just like my favorite tortilla soup from BJs. I am sending this recipe to my family. I want everyone to try it!',
+                profilePic: 'images/profile.png',
+                level: 'Novice',
+                bio: 'Food enthusiast. Love to cook and bake.'
+            },
+            post2: {
+                author: 'LINH',
+                date: 'February 15, 2021 @ 9:46 am',
+                text: 'I just made this soup today and it’s so tasty! didn’t have corn at home but still turned out very good.  It’s a winner!  I made beef chili for my parents; but since my dad has gout he can’t eat beef; this white chicken chili is perfect for him.  Thank you Lisa!',
+                profilePic: 'images/profile.png',
+                level: 'Newcomer',
+                bio: 'Love food! Exploring new recipes every day.'
+            },
+            post3: {
+                author: 'CATHERINE LEONARDO',
+                date: 'February 13, 2021 @ 12:58 pm',
+                text: 'I LOVE this White Chicken Chili! You are right, it is satiating meal—delicious with toasted bread. Refreshingly different taste than any chicken chili I’ve made in the past. I made it exactly as written and added some chopped zucchini, carrots, and celery. Instead of shredding the chicken, I chopped it into small pieces. It freezes very well. Will be an all-time favorite, for sure.',
+                profilePic: 'images/profile.png',
+                level: 'Mentor',
+                bio: 'I have to say I never was the adventurous type until 2 years ago. Now I share my food adventures with everyone!'
+            },
+            post4: {
+                author: 'KALI',
+                date: 'February 13, 2021 @ 11:31 am',
+                text: 'This recipe is dynamite! My partner usually won’t eat beans but he finished the whole pot (darn was hoping to have some for leftovers haha). This is crowd-pleaser that I am going to add to my regular recipe rotation. Thanks so much, Lisa!',
+                profilePic: 'images/profile.png',
+                level: 'Novice',
+                bio: 'Food is my passion. So is cooking.'
+            }
+        }
+    },
+    methods: {
+        displayProfile: function(post) {
+            this.activePost = post;
+        }
+    }
+});
